0bf973259603500ab86b41fa10972aa0925e9d8a
lmbedford <67244010+lmbedford@users.noreply.github.com>
2023-07-10 22:35:29 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import SkillsSection from './components/SkillsSection';
+import ExperienceSection from './components/ExperienceSection';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <SkillsSection />
+      <ExperienceSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
